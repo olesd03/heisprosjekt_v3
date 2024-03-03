39a41e142cbb7e7fcc2dd6f214a59177701e6b99
olesd03 <PcNice2@outlook.com>
2024-03-03 22:04:53 +0100

    Lagde en ny versjon av heisprosjektet, som tar høy de for emergency stop.

--- lab_2-main/skeleton_project/source/main.c
+++ lab_2-main/skeleton_project/source/main.c
@@ -4,47 +4,208 @@
 #include <time.h>
 #include "driver/elevio.h"
 
+typedef enum {
+    down=-1,
+    up=0,
+    idle=1,
+    emstop=2
+} state;
+
+typedef enum {
+    false = 0,
+    true = 1
+} bool;
+
+void GoUpToClosest(void) {
+    while (elevio_floorSensor() == -1) {
+        elevio_motorDirection(DIRN_UP); 
+    }
+}
+
+bool BreakAndEmstop(state *elev_state) {
+    if (elevio_stopButton()) {
+        *elev_state = emstop;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void UpdateCurrentFloor(int *current_floor) {
+    if (elevio_floorSensor() != -1) {
+        *current_floor = elevio_floorSensor() +1;
+    }
+}
 
 
 int main(){
     elevio_init();
-    
-    printf("=== Example Program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
+    goUpToClosest();
 
-    elevio_motorDirection(DIRN_UP);
+    state elev_state = idle;
+    bool floor_stop;
+    bool orders_empty;
+    bool current_floor_in_orders;
+    int current_floor;
+    int maxOrder = 0;
+    int minOrder = 0;
+    char orderArray[4];
+
 
     while(1){
-        int floor = elevio_floorSensor();
-
-        if(floor == 0){
-            elevio_motorDirection(DIRN_UP);
-        }
-
-        if(floor == N_FLOORS-1){
+        while(elev_state == down) {
+            nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+            if (breakAndEmstop(&elev_state)) {break;}
             elevio_motorDirection(DIRN_DOWN);
+            UpdateCurrentFloor(&current_floor);
+            AddOrders(orderArray);
+            
+
+            if (elevio_floorSensor() != -1) {
+                if (current_floor == minOrder) {
+                    StopAndLight();  // Add later
+                    UpdateMinAndMaxOrder(orderArray, &minOrder, &maxOrder); // Add later
+                    if (current_floor > minOrder) {
+                        elev_state = down;
+                    }
+                    else {
+                        if (current_floor < maxOrder) {
+                            elev_state = up;
+                        }
+                        else {
+                            elev_state = idle;
+                        }
+                    }
+                }
+                else {
+                    UpdateFloorStop(orderArray, &current_floor); // Add later
+                    if (floor_stop) {
+                        StopAndLight();
+                    }
+                }
+            }
         }
+        while(elev_state == up) {
+            nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+            if (breakAndEmstop(&elev_state)) {break;}
+            elevio_motorDirection(DIRN_UP);
+            UpdateCurrentFloor(&current_floor);
+            AddOrders(orderArray);
+            
 
-
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed);
+            if (elevio_floorSensor() != -1) {
+                if (current_floor == maxOrder) {
+                    StopAndLight();
+                    UpdateMinAndMaxOrder(orderArray, &minOrder, &maxOrder);
+                    if (current_floor < maxOrder) {
+                        elev_state = up;
+                    }
+                    else {
+                        if (current_floor > minOrder) {
+                            elev_state = down;
+                        }
+                        else {
+                            elev_state = idle;
+                        }
+                    }
+                }
+                else {
+                    UpdateFloorStop(orderArray, &orderArray);
+                    if (floor_stop) {
+                        StopAndLight();
+                    }
+                }
+            }
+        }
+        while(elev_state == idle) {
+            nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+            if (breakAndEmstop(&elev_state)) {break;}
+            GoUpToClosest();
+            UpdateOrdersEmpty(); //Add later
+            if (!orders_empty) { 
+                UpdateCurrentInOrders(); //Add later
+                if (current_floor_in_orders) {
+                    StopAndLight();
+                }
+                else {
+                    if (current_floor > minOrder) {
+                        elev_state = down;
+                    }
+                    else {
+                        elev_state = up;
+                    }
+                }
             }
         }
 
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
-        
-        if(elevio_stopButton()){
+
+
+
+        label_emstop:
+        while(elev_state == emstop) {
+            DeleteOrders();
+            UpdateOrdersEmpty();
             elevio_motorDirection(DIRN_STOP);
-            break;
+
+            if (elevio_floorSensor() != -1) {
+                UpdateCurrentFloor(&current_floor);
+
+                elevio_stopLamp(true);
+                double Time = 3;
+                while (Time >= 0) {
+                    if (elevio_obstruction()) {
+                        Time = 3;
+                    }
+                    else {
+                        Time -= 0.01;
+                    }
+                    nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+                    if (elevio_stopButton()) {
+                        goto label_emstop;
+                    }
+                    else {
+                        AddOrders(); // Add later
+                    }
+                }
+                elevio_stopLamp(false);
+
+                while (orders_empty) {
+                    AddOrders();          // Add later
+                    UpdateOrdersEmpty();  // Add later
+                    nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+                    if (elevio_stopButton()) {
+                        goto label_emstop;
+                    }
+                }
+
+                UpdateCurrentFloorInOrders(); // Add later
+                UpdateMinAndMaxOrder();
+                if (current_floor_in_orders) {
+                    elev_state = idle;
+                }   
+                else {
+                    if (current_floor < maxOrder) {
+                        elev_state = up;
+                    }
+                    else {
+                        elev_state = down;
+                    }
+                }
+            }
+            else {
+                UpdateOrdersEmpty(orderArray);
+                while (orders_empty) {
+                    nanosleep(&(struct timespec){0, 10*1000*1000}, NULL);
+                    if (elevio_stopButton()) {
+                        goto label_emstop;
+                    }
+                    AddOrders(orderArray); // Add later
+                    UpdateOrdersEmpty(orderArray, &orders_empty); // Add later
+                }
+                ChangeStateBetween(&elev_state); // Add later
+            }
         }
-        
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
     return 0;
